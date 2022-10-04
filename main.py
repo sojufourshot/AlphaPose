abbf7cf1224077e728bcbe4d992390b7d8ffc0f3
riroan <riroan@naver.com>
2022-10-05 08:15:38 +0900

    ai/feat: 점수 계산 api 추가

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import os
+import sys
 from fastapi import FastAPI
+from scripts.utility import get_score
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/scripts")
 
 app = FastAPI()
 
+
 @app.get("/")
-async def root():
-    return {"message":"Hello World"}
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/api/v1/score/{src1}/{src2}")
+def func(src1: str, src2: str):
+    score = get_score(src1, src2)
+    return {"score": score}
